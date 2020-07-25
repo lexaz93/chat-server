f2e8364d4c65fcdc58666b4c7678530fda47a0a4
lexaz93 <lexaz93@mail.ru>
2020-07-25 14:55:05 +0400

    Перезалил

--- src/main/java/services/Observable.java
+++
@@ -1,7 +0,0 @@
-package services;
-
-public interface Observable {
-    void addObserver(Observer o);
-    void deleteObserver(Observer o);
-    void notifyObservers(String message);
-}
--- src/main/java/services/Observer.java
+++
@@ -1,5 +0,0 @@
-package services;
-
-public interface Observer {
-    void notifyObserver(String message);
-}
